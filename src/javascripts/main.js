e0d4200589a2aee059743d05238821154ce731bf
tak <69186176+hotakuy@users.noreply.github.com>
2021-11-03 12:19:36 +0900

    ES6, React, Vue

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,5 +1,14 @@
+import './reactApp.jsx';
 import my from './my.js';
 import '../stylesheets/main.scss';
+
+import Vue from 'vue';
+import vueApp from './vueApp.vue';
+
+new Vue({
+  el: '#vue-root',
+  render: (h) => h(vueApp),
+});
 
 console.log('webpack!');
 my();
