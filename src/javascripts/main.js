e197435dc669072508685e7feb3c5a4186daaabf
tak <69186176+hotakuy@users.noreply.github.com>
2021-11-03 16:25:37 +0900

    Install Typescript

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,10 +5,13 @@ import '../stylesheets/main.scss';
 import Vue from 'vue';
 import vueApp from './vueApp.vue';
 
+import add from './add.ts';
+
 new Vue({
   el: '#vue-root',
   render: (h) => h(vueApp),
 });
 
+console.log(add(3, 9));
 console.log('webpack!');
 my();
